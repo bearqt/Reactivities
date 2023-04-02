0195272cf6c9a47fed0ec81aa9485e9e7643d455
bearqt <feikfeikoviy@mail.ru>
2023-04-02 15:17:36 +0300

    End of section 16

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,5 +13,6 @@ namespace Domain
         public string Bio { get; set; }
 
         public ICollection<ActivityAttendee> Activities { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
