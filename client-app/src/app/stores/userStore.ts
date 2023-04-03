b60575dca3872a30aff71e6fb5a0602eafdf723b
bearqt <feikfeikoviy@mail.ru>
2023-04-03 17:19:29 +0300

    End of section 17

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,20 +1,20 @@
-﻿import {User, UserFormValues} from "../models/user";
-import {makeAutoObservable, runInAction} from "mobx";
+﻿import { User, UserFormValues } from "../models/user";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
-import {store} from "./store"
-import {history} from '../../index';
+import { store } from "./store"
+import { history } from '../../index';
 
 export default class UserStore {
     user: User | null = null;
-    
+
     constructor() {
         makeAutoObservable(this);
     }
-    
+
     get isLoggedIn() {
         return !!this.user;
     }
-    
+
     login = async (creds: UserFormValues) => {
         try {
             const user = await agent.Account.login(creds);
@@ -22,26 +22,26 @@ export default class UserStore {
             runInAction(() => this.user = user);
             history.push('/activities');
             store.modalStore.closeModal();
-            
-        } catch(error) {
+
+        } catch (error) {
             throw error;
         }
     }
-    
+
     logout = () => {
         store.commonStore.setToken(null);
         window.localStorage.removeItem('jwt');
         this.user = null;
         history.push('/');
     }
-    
+
     getUser = async () => {
         try {
             const user = await agent.Account.current();
             runInAction(() => {
                 this.user = user
             });
-            
+
         } catch (error) {
             console.log(error);
         }
@@ -54,9 +54,15 @@ export default class UserStore {
             runInAction(() => this.user = user);
             history.push('/activities');
             store.modalStore.closeModal();
-            
-        } catch(error) {
+
+        } catch (error) {
             throw error;
         }
+    }
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
+        
+
     }
 }
