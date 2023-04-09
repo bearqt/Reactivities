9e2a6520981286880a9cf70699c218052c587f61
bearqt <feikfeikoviy@mail.ru>
2023-04-09 17:33:57 +0300

    End of section 21

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,7 +5,8 @@ import { Activity, ActivityFormValues } from '../models/activity';
 import { store } from '../stores/store';
 import { User, UserFormValues } from "../models/user";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import { Photo, Profile } from '../models/profile';
+import { Photo, Profile, UserActivity } from '../models/profile';
+import { PaginatedResult } from '../models/pagination';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -23,6 +24,11 @@ axios.interceptors.request.use(config => {
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginatedResult<any>>;
+    }
     return response;
 
 }, (error: any) => {
@@ -70,7 +76,8 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
+    list: (params: URLSearchParams) => axios.get<PaginatedResult<Activity[]>>('/activities', { params })
+        .then(responseBody),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
     update: (activity: ActivityFormValues) => axios.put<void>(`/activities/${activity.id}`, activity),
@@ -97,7 +104,9 @@ const Profiles = {
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
     updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
     listFollowings: (username: string, predicate: string) => requests
-        .get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+        .get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+    listActivities: (username: string, predicate: string) =>
+        requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 
